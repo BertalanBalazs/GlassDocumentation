ad8c10108e2a1db5b1851b069b1dad1ad0c28fa2
Gabor Buki <gabor.bki@gmail.com>
2019-09-03 13:54:36 +0200

    Built basic project structure for Cucumber.

---
+++ src/test/java/glass/MyStepdefs.java
@@ -0,0 +1,20 @@
+package glass;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+
+    @Given("I don't know")
+    public void today_is(String today) {
+    }
+
+    @When("I have no idea")
+    public void i_ask_whether_it_s_Friday_yet() {
+    }
+
+    @Then("I know nothing")
+    public void i_should_be_told(String expectedAnswer) {
+    }
+}
---
+++ src/test/resource/glass/demo.feature
@@ -0,0 +1,6 @@
+Feature: Demo Feature
+
+  Scenario: I don't know
+    Given I don't know
+    When I have no idea
+    Then I know nothing
