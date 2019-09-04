70179e6fb278ed756529388a53797f71076296e4
gaborTachtler <tachtler@gmail.com>
2019-09-04 11:37:16 +0200

    add navigateToBasePage method

--- src/main/java/com/codecool/tw6/glass/pages/BasePageObject.java
+++ src/main/java/com/codecool/tw6/glass/pages/BasePageObject.java
@@ -1,7 +1,10 @@
 package com.codecool.tw6.glass.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePageObject {
     private static final String BROWSER = System.getenv("BROWSER");
@@ -17,7 +20,21 @@ public class BasePageObject {
         return BROWSER;
     }
 
+    public void navigateToGlassSummaryPage(){
+        driver.get("https://jira2.codecool.codecanvas.hu/projects/DEMO?selectedItem=com.codecanvas.glass:glass");
+    }
+
     public void navigate(String location){
         driver.get(location);
+    }
+
+    // project can be either: DEMO or ST
+    public void openProjectPage(String project){
+        driver.get("https://jira2.codecool.codecanvas.hu/projects/" + project + "?selectedItem=com.codecanvas.glass:glass");
+    }
+
+    public void waitForElement(WebElement element, int waitTime){
+        WebDriverWait wait = new WebDriverWait(driver, waitTime);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
