6a0b368f8923912a56ebf274fdc71f18567247fa
Gabor Buki <gabor.bki@gmail.com>
2019-09-04 10:55:59 +0200

    Created scenarios in feature and stepdefs.

--- src/test/java/glass/WorkflowStepDefs.java
+++ src/test/java/glass/WorkflowStepDefs.java
@@ -7,28 +7,68 @@ import cucumber.api.java.en.When;
 
 public class WorkflowStepDefs {
 
-    @Given("I am logged in to the {string} with admin privileges.")
-    public void iAmLoggedInToTheWithAdminPrivileges(String website) {
-
-
+    @Given("I visit the login page")
+    public void iVisitTheLoginPage() {
     }
 
-    @When("I visit the following {string}")
+    @When("I enter my username and password")
+    public void iEnterMyUsernameAndPassword() {
+    }
+
+    @Then("I should be logged in")
+    public void iShouldBeLoggedIn() {
+    }
+
+    @Given("I visit the following {string}")
     public void iVisitTheFollowing(String link) {
-
-
     }
 
-    @And("I click on the Add workflow menu")
+    @When("I click on the Add workflow menu")
     public void iClickOnTheAddWorkflowMenu() {
-
-
     }
 
     @Then("a new prompt shows up that asks for a Name and a Description for a new project")
     public void aNewPromptShowsUpThatAsksForANameAndADescriptionForANewProject() {
-
-
     }
 
+    @Given("I am on {string}")
+    public void iAmOn(String url) {
+    }
+
+    @When("I click on Issue Types menu")
+    public void iClickOnIssueTypesMenu() {
+    }
+
+    @And("I select TestIssue")
+    public void iSelectTestIssue() {
+    }
+
+    @Then("a Workflow box shows the workflow associated with TestIssue")
+    public void aWorkflowBoxShowsTheWorkflowAssociatedWithTestIssue() {
+    }
+
+    @Given("I am on <link>")
+    public void iAmOnLink() {
+    }
+
+    @When("I clik on the Issue Types")
+    public void iClikOnTheIssueTypes() {
+    }
+
+    @And("I click on TestIssue")
+    public void iClickOnTestIssue() {
+    }
+
+    @Then("I see a Workflow Transitions box with the following elements: Create, Start Progress, Review Problem, Finish Issue, Close Issue")
+    public void iSeeAWorkflowTransitionsBoxWithTheFollowingElementsCreateStartProgressReviewProblemFinishIssueCloseIssue() {
+    }
+
+
+    @Given("I visit {string}")
+    public void iVisit(String url) {
+    }
+
+    @Then("{string} appears on the page")
+    public void appearsOnThePage(String workflowName) {
+    }
 }
--- src/test/resource/glass/Workflow.feature
+++ src/test/resource/glass/Workflow.feature
@@ -1,10 +1,40 @@
-Feature: Create a new workflow as an admin
+Feature: Create and handle workflows as an admin
+
+  Background: User is logged in as admin
+    Given I visit the login page
+    When I enter my username and password
+    Then I should be logged in
 
   Scenario Outline: To check if it is possible to create a new workflow when logged in as an admin.
-    Given I am logged in to the "<website>" with admin privileges.
-    When I visit the following "<link>"
-    And I click on the Add workflow menu
+    Given I visit the following "<link>"
+    When I click on the Add workflow menu
     Then a new prompt shows up that asks for a Name and a Description for a new project
 
-    Examples: website | link
-      | jira2.codecool.codecanvas.hu | https://jira2.codecool.codecanvas.hu/secure/admin/workflows/ListWorkflows.jspa |
+    Examples: link
+      | https://jira2.codecool.codecanvas.hu/secure/admin/workflows/ListWorkflows.jspa |
+
+
+    Scenario Outline: Check the correct workflow is shown
+      Given I am on "<url>"
+      When I click on Issue Types menu
+      And I select TestIssue
+      Then a Workflow box shows the workflow associated with TestIssue
+
+      Examples: url
+        | https://jira2.codecool.codecanvas.hu/projects/DEMO?selectedItem=com.codecanvas.glass:glass |
+
+  Scenario Outline: Check that the workflow transitions are correctly shown on issue page
+      Given I am on "<url>"
+      When I clik on the Issue Types
+      And I click on TestIssue
+      Then I see a Workflow Transitions box with the following elements: Create, Start Progress, Review Problem, Finish Issue, Close Issue
+
+      Examples: url
+        | https://jira2.codecool.codecanvas.hu/projects/DEMO?selectedItem=com.codecanvas.glass:glass |
+
+    Scenario Outline: Workflow appears on the settings page outside of glass
+      Given I visit "<url>"
+      Then "<custom workflow name>" appears on the page
+
+      Examples: url
+        | https://jira2.codecool.codecanvas.hu/plugins/servlet/project-config/DEMO/workflows
