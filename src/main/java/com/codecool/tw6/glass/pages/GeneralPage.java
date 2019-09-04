c8e8a18c5d86f71fdde34b9ea1eaaeef4ce39cfc
Geiszlinger Gergely <ggergely@gmail.com>
2019-09-04 10:12:38 +0200

    Constructor for general page done

--- src/main/java/com/codecool/tw6/glass/pages/GeneralPage.java
+++ src/main/java/com/codecool/tw6/glass/pages/GeneralPage.java
@@ -1,4 +1,12 @@
 package com.codecool.tw6.glass.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class GeneralPage extends BasePageObject{
+
+    public GeneralPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
 }
