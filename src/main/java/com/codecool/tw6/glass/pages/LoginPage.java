8e6a53ef29c36cd2dfe3f86830945cb0a813f566
Gabor Buki <gabor.bki@gmail.com>
2019-09-04 13:06:34 +0200

    LoginPage login method no longer static due to PageFactory limitations.

--- src/main/java/com/codecool/tw6/glass/pages/LoginPage.java
+++ src/main/java/com/codecool/tw6/glass/pages/LoginPage.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     private static String url = "https://jira2.codecool.codecanvas.hu/login.jsp";
-    private static WebDriver driver = BrowserFactory.getWebDriver(System.getenv("BROWSER"));
-    private static WebDriverWait wait = new WebDriverWait(driver, 10);
+    private WebDriver driver;
+    private WebDriverWait wait;
 
 
     @FindBy(id = "login-form-username")
@@ -23,7 +24,13 @@ public class LoginPage {
     @FindBy(id = "logo")
     private static WebElement logo;
 
-    public static void login() {
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login() {
         driver.get(url);
         wait.until(ExpectedConditions.visibilityOf(username)).sendKeys(System.getenv("USERNAME"));
         wait.until(ExpectedConditions.visibilityOf(password)).sendKeys(System.getenv("PASSWORD"));
