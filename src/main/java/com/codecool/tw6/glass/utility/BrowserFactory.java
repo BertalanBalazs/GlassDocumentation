79297093e72843e0948ad9a5e8337ca316750254
erika <erikatoth.0831@gmail.com>
2019-09-04 14:16:21 +0200

    BrowserFactory: updated properties

--- src/main/java/com/codecool/tw6/glass/utility/BrowserFactory.java
+++ src/main/java/com/codecool/tw6/glass/utility/BrowserFactory.java
@@ -10,8 +10,10 @@ public class BrowserFactory {
 
         switch (browser) {
             case "CHROME":
+                System.setProperty("webdriver.chrome.driver", System.getenv("DRIVER_LOCATION"));
                 return new ChromeDriver();
             case "FIREFOX":
+                System.setProperty("webdriver.gecko.driver", System.getenv("DRIVER_LOCATION"));
                 return new FirefoxDriver();
             case "IE":
                 throw new IllegalArgumentException("C'mon nobody uses IE");
