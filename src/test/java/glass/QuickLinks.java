af77e8fb62a10f347989f7c819872f17de997bbc
erika <erikatoth.0831@gmail.com>
2019-09-04 10:43:36 +0200

    General page quick link test Cucumber skeleton

--- src/test/java/glass/QuickLinks.java
+++ src/test/java/glass/QuickLinks.java
@@ -1,4 +1,29 @@
 package glass;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class QuickLinks {
+
+    /*
+    Scenario: General page quick link test
+     */
+
+    @Given("I am logged in as system admin")
+    public void iAmLoggedInAsSystemAdmin() {
+    }
+
+    @And("I am on the Basic summary page")
+    public void iAmOnTheBasicSummaryPage() {
+    }
+
+    @When("I click on the quick link next to the text Basic summary")
+    public void iClickOnTheQuickLinkNextToTheTextBasicSummary() {
+    }
+
+    @Then("the Project settings Details opens in a new window")
+    public void theProjectSettingsDetailsOpensInANewWindow() {
+    }
 }
