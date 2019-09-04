db962c337c8d0a04a500a8fe5c1d2f8b379548c4
erika <erikatoth.0831@gmail.com>
2019-09-04 11:12:45 +0200

    Renamed QuickLinkStepDefs class

--- src/test/java/glass/QuickLinkStepDefs.java
+++ src/test/java/glass/QuickLinkStepDefs.java
@@ -5,7 +5,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class QuickLinks {
+public class QuickLinkStepDefs {
 
     /*
     Scenario: General page quick link test
