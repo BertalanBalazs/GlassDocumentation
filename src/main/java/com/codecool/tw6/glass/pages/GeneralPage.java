58ccd8780ed0023dca262293afd301320c6dc255
Bence <arvai.bence1@gmail.com>
2019-09-05 19:01:49 +0200

    Add selectors and keywords for scheme chagnge validation on GeneralPage

--- src/main/java/com/codecool/tw6/glass/pages/GeneralPage.java
+++ src/main/java/com/codecool/tw6/glass/pages/GeneralPage.java
@@ -1,12 +1,32 @@
 package com.codecool.tw6.glass.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GeneralPage extends BasePageObject{
+
+    @FindBy(xpath = "//a[@id='aui-uid-3']")
+    private WebElement schemesTabLink;
+
+    //get current scheme, which is the first td next to the scheme title
+    @FindBy(xpath = "//td[@class='project-meta-label'][contains(.,'Issue Type Scheme')]//following-sibling::td[1]")
+    private WebElement currentIssueTypeSchemeCell;
 
     public GeneralPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
+
+    public void clickSchemesTabLink(){
+        waitForElement(schemesTabLink);
+        schemesTabLink.click();
+    }
+
+    public String getCurrentIssueTypeSchemeText(){
+        waitForElement(currentIssueTypeSchemeCell);
+        return currentIssueTypeSchemeCell.getText();
+    }
+
 }
